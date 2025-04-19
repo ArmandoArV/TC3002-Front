d34003b594b34a85498752a6ffb1fc393d97e86b
ArmandoArV <armando.arredondo.valle@gmail.com>
2025-04-19 13:10:18 -0600

    Implements suspense

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,12 +2,16 @@
 import HeaderComponent from "./Components/HeaderComponent/HeaderComponent";
 import HomeClientWrapper from "./Components/Containers/HomeClientWrapper/home-client-wrapper";
 import styles from "./page.module.css";
-
+import { Suspense } from "react";
 export default function Home() {
   return (
     <div>
       <HeaderComponent />
-      <HomeClientWrapper />
+      <div className={styles.mainContainer}>
+        <Suspense fallback={<div>Loading...</div>}>
+          <HomeClientWrapper />
+        </Suspense>
+      </div>
     </div>
   );
 }
