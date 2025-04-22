1d9d0b86474f1f96a7eb80236088bc0bd092d976
ArmandoArV <armando.arredondo.valle@gmail.com>
2025-04-22 12:33:54 -0600

    Implements domain

--- next.config.ts
+++ next.config.ts
@@ -3,7 +3,7 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   reactStrictMode: true,
   images: {
-    domains: ["dermatoss-images-bucket.s3.us-east-1.amazonaws.com"], // Add your hostname here
+    domains: ["dermatoss-images-bucket.s3.us-east-1.amazonaws.com", "dermatoss-images-bucket.s3.amazonaws.com"], // Corrected hostnames
   },
 };
 
